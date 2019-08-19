ad8347f0a0387bb9e9e6156d3815223d9fbfd202
Nyapaw <45115274+Nyapaw@users.noreply.github.com>
2019-08-18 19:49:13 -0700

    Update QuickSort.lua

--- Sorting Algorithms/QuickSort.lua
+++ Sorting Algorithms/QuickSort.lua
@@ -34,40 +34,4 @@ quick.sort = function(arr, callback)
     return arr
 end;
 
-return quick;-- LR pointers
-
-local quick = {};
-
-local function partition(arr, low, high, callback)
-    local pivot = arr[high]; -- given arr[low] -> arr[high], splits the array between this pivot.
-
-    local j = low - 1;
-
-    for i = low, high - 1 do
-        if callback and callback(arr[i], pivot) or arr[i] < pivot then
-            j = j + 1;
-            arr[i], arr[j] = arr[j], arr[i];
-        end;
-    end;
-
-    arr[j + 1], arr[high] = arr[high], arr[j + 1]; -- swap high with the "median" of the two arrays
-
-    return j + 1; -- Returns the index splitting the two sorted partitions
-end;
-
-local function sort(arr, low, high, callback)
-	if (low >= high) then return end;
-	
-    local i = partition(arr, low, high, callback);
-
-    sort(arr, low, i - 1, callback);
-    sort(arr, i + 1, high, callback);
-end;
-
-quick.sort = function(arr, callback)
-	sort(arr, 1, #arr, callback);
-	
-    return arr
-end;
-
 return quick;
