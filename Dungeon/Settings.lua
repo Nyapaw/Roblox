0845e199a0dea247a66d5b8168056a2d268130b2
Nyapaw <45115274+Nyapaw@users.noreply.github.com>
2019-09-01 09:56:21 -0700

    Create Settings.lua

---
+++ Dungeon/Settings.lua
@@ -0,0 +1,12 @@
+return {
+	
+	MAX_ROOM_TRIES 	= 300;
+	MAX_ROOMS		= 3;
+	
+	MIN_ROOM_SIZE 	= {x = 3, y = 3};
+	MAX_ROOM_SIZE	= {x = 8, y = 8};
+	
+	ROOM_BORDER_LENIANCY = 3;
+	ROOM_BORDER_RETRIES = 10;
+	
+};
